e9b7ae1819e06ebe06b523422adf13f4f6f99a6b
Muhammad Norfirdaus Bin Noorhisham <muhdnorfirdaus27@gmail.com>
2024-04-22 20:20:47 +0800

    New update

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body class="bg-blue-400">
+    <h1 class="font-bold text-white text-4xl text-center">Welcome to index</h1>
+</body>
+</html>
