f896f349710ade7eeeac7621fcbf31b18960fedb
Muhammad Norfirdaus Bin Noorhisham <muhdnorfirdaus27@gmail.com>
2024-04-24 09:18:45 +0800

    latest update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -13,4 +15,18 @@ class ProductController extends Controller
     public function create() {
         return view('product.create');
     }
+
+    public function store(Request $request) {
+        $data = $request->validate([
+            'name' => 'required',
+            'quantity' => 'required|numeric',
+            'price' => 'required|decimal:2',
+            'description' => 'nullable',
+        ]);
+
+        $newProduct = Product::create($data);
+
+        return redirect(route('product.index'));
+    }
+
 }
--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -7,5 +7,31 @@
 </head>
 <body>
     <h1>Create Products</h1>
+
+    <form action="{{route('product.store')}}" method="POST">
+        @csrf
+        @method('post')
+
+        <div>
+            <label for="name">Name :</label>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label for="quantity">Quantity :</label>
+            <input type="text" name="quantity">
+        </div>
+        <div>
+            <label for="price">Price :</label>
+            <input type="text" name="price">
+        </div>
+        <div>
+            <label for="description">Description :</label>
+            <input type="text" name="description">
+        </div>
+
+        <div>
+            <input type="submit" value="Save a New Product">
+        </div>
+    </form>
 </body>
 </html>
